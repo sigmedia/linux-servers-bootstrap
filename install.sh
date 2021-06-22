68df89852f0725295d53104a03b87027c3ff3bbd
Sebastien Le Maguer <lemagues@tcd.ie>
2021-06-22 09:22:38 +0100

    [install] add: starting services

--- install.sh
+++ install.sh
@@ -4,7 +4,16 @@
 pacman -Syu
 pacman -S --noconfirm $(grep -v "^\(^#.*\|[ \t]*\)$" package_list.conf | sed 's/[ ]*#.*//g' | tr '\n' ' ')
 
+# Configure important part of the system
+#  - Start services
+#    [FIXME: names are currently are coded there, we should be able to retrieve that from somewhere]
+services_to_start=(dhcpcd sshd)
+for service in ${services_to_start[@]}; do
+    systemctl enable $service
+    systemctl start $service
+done
+
 # Achieve post processing
 for i in $(ls -d post_processing/*); do
-	echo $i/install.sh
+    bash $i/install.sh
 done
