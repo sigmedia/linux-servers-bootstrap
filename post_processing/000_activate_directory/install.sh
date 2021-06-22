9d5f5d4e315eb5d833e8615af29b88c39d71e1a8
Sebastien Le Maguer <lemagues@tcd.ie>
2021-06-22 09:12:10 +0100

    [post_processing] add: active directory installation support

---
+++ post_processing/000_activate_directory/install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+## Automatisation of the process described in https://wiki.archlinux.org/title/Active_Directory_integration
+
+# 1. Configure resolv, ntp, samba, kerberos and nsswitch
+if [ -f /etc/resolv.conf ]; then
+    cp /etc/resolv.conf /etc/resolv.conf.backup
+fi
+cp templates/resolv.conf /etc/
+
+if [ -f /etc/ntp.conf ]; then
+    cp /etc/ntp.conf /etc/ntp.conf.backup
+fi
+cp templates/ntp.conf /etc/
+
+if [ -f /etc/samba/smb.conf ]; then
+    cp /etc/samba/smb.conf /etc/samba/smb.conf.backup
+fi
+cp templates/smb.conf /etc/samba/
+
+if [ -f /etc/krb5.conf ]; then
+    cp /etc/krb5.conf /etc/krb5.conf.backup
+fi
+cp templates/krb5.conf /etc/
+
+if [ -f /etc/nsswitch.conf ]; then
+    cp /etc/nsswitch.conf /etc/nsswitch.conf.backup
+fi
+cp templates/nsswitch.conf /etc/
+
+# 2. Configure PAM
+#   - General WinBind configuration
+if [ -f /etc/security/pam_winbind.conf ]; then
+    cp /etc/security/pam_winbind.conf /etc/security/pam_winbind.conf.backup
+fi
+cp templates/pam_winbind.conf /etc/security/
+
+#   - Adapt system authentication (/etc/pam.d/system-auth)
+if [ -f /etc/pam.d/system-auth ]; then
+    cp /etc/pam.d/system-auth /etc/pam.d/system-auth.backup
+fi
+sed -i '/^auth[ \t]*required[ \t]*pam_unix.so/i auth\tsufficient\tpam_winbind.so' /etc/pam.d/system-auth
+sed -i '/^account[ \t]*required[ \t]*pam_unix.so/i account\tsufficient\tpam_winbind.so' /etc/pam.d/system-auth
+sed -i '/^password[ \t]*required[ \t]*pam_unix.so/i password\tsufficient\tpam_winbind.so' /etc/pam.d/system-auth
+sed -i '/^session[ \t]*required[ \t]*pam_unix.so/i session\tsufficient\tpam_winbind.so' /etc/pam.d/system-auth
+
+# #   - Adapt user change (/etc/pam.d/su) [FIXME: May not be useful]
+# if [ -f /etc/pam.d/su ]; then
+#     cp /etc/pam.d/su /etc/pam.d/su.backup
+# fi
+# sed -i '/^auth[ \t]*required[ \t]*pam_unix.so/i auth\tsufficient\tpam_winbind.so' /etc/pam.d/su
+# sed -i '/^account[ \t]*required[ \t]*pam_unix.so/i account\tsufficient\tpam_winbind.so' /etc/pam.d/su
+# sed -i '/^password[ \t]*required[ \t]*pam_unix.so/i password\tsufficient\tpam_winbind.so' /etc/pam.d/su
+# sed -i '/^session[ \t]*required[ \t]*pam_unix.so/i session\tsufficient\tpam_winbind.so' /etc/pam.d/su
+
+# 3. Configure SSH
+if [ -f /etc/ssh/sshd_config ]; then
+    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+fi
+
+echo "
+# Change to no to disable s/key passwords
+ChallengeResponseAuthentication no
+
+# Kerberos options
+KerberosAuthentication yes
+#KerberosOrLocalPasswd yes
+KerberosTicketCleanup yes
+KerberosGetAFSToken yes
+
+# GSSAPI options
+GSSAPIAuthentication yes
+GSSAPICleanupCredentials yes
+" | tee -a /etc/ssh/sshd_config > /dev/null
+
+# 4. Configure SUDO
+if [ -f /etc/sudoers ]; then
+    cp /etc/sudoers /etc/sudoers.backup
+fi
+sed -i 's/^#[ \t]*%sudo/%sudo/g' /etc/sudoers
+sed -i '/^%sudo/i %supermedia ALL=(ALL) ALL' /etc/sudoers
+
+# 5. Start services
+systemctl enable smb.service
+systemctl start smb.service
+
+systemctl enable nmb.service
+systemctl start nmb.service
+
+systemctl enable winbind.service
+systemctl start winbind.service
+
+systemctl enable ntpd
+systemctl start ntp
+
+systemctl enable sshd
+systemctl start sshd
