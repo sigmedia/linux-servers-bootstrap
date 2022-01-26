bc0a0d0858d8e3e0ad33eb07f37c9fdfcdb2420d
Mark Anderson <andersm3@tcd.ie>
2022-01-26 14:09:17 +0000

    [userspace] nvidia-container toolkit setup

--- 02_userspace_install.sh
+++ 02_userspace_install.sh
@@ -20,6 +20,17 @@ echo "## Install packages from the AUR"
 echo "==========================================================="
 yay -S $(grep -v "^\(^#.*\|[ \t]*\)$" packages/user.conf | sed 's/[ ]*#.*//g' | tr '\n' ' ')
 
+# Configure nvidia-container-toolkit
+echo "==========================================================="
+echo "## Setting up nvidia-container-toolkit"
+echo "==========================================================="
+sudo sed -i 's/^no-cgroups =.*/no-cgroups = false/' /etc/nvidia-container-runtime/config.toml
+sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/$/ systemd.unified_cgroup_hierarchy=false/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
 echo "==========================================================="
 echo "## DONE :)"
 echo "==========================================================="
+echo "/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\"
+echo "It is recommended to reboot a last time"
+echo "/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\"
